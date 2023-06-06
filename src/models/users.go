3851effe91a07e7a01790a63d724c64ab9343385
Alaa Barka <tmakaveli643@gmail.com>
2023-06-06 01:43:50 +0100

    model

---
+++ src/models/users.go
@@ -0,0 +1,20 @@
+package models 
+
+
+
+import (
+	
+	"gorm.io/gorm"
+)
+
+
+type Users struct {
+	
+	id int `gorm:"primaryKey;autoIncrement"`
+	fullname string
+	email string `gorm:"unique"`
+	password string
+	phone string `gorm:"unique"`
+
+	created_at string
+}
