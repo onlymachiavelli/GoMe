0ea50fd673d9a77f89f16c0d16b46bb8482a9d1d
Alaa Barka <tmakaveli643@gmail.com>
2023-06-06 01:15:11 +0100

    connect PSQL

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 import (
 	"fmt"
+	//"helper"
+	//"utils"
+	"github.com/joho/godotenv"	
+	"os"
+
 )
 
 
 func main () {
 	fmt.Println("hello")
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("error loading .env file")
+	}
+	//printing TST data from dotenv 
+	fmt.Println("TST: ", os.Getenv("TST"))
 }
--- src/utils/Connect.go
+++ src/utils/Connect.go
@@ -3,10 +3,31 @@ package connector
 
 import (
 	"fmt"
+	"os"
+	"github.com/joho/godotenv"
+	"helper"
+	"gorm.io/driver/postgres"
+  	"gorm.io/gorm"	
 
 )
 
 func Connect() {
 
-	return nil 
+	err := godotenv.Load()
+	if (err != nil) {
+		helper.errorHandler(err)
+	}
+	HOST := os.Getenv("HOST")
+	USER := os.Getenv("USER")
+	PORT:= os.Getenv("DBPORT")
+	PASS := os.Getenv("PASS")
+	DBNAME := os.Getenv("DBNAME")
+
+
+	conn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d", HOST, USER,PASS, DBNAME,PORT)
+
+	db, err := gorm.Open(postgres.Open(conn), &gorm.Config{})
+
+	return db, err
+
 }
