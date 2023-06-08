a226dce1b00180a4d5514f49741c9dcdc13ac9e2
Alaa Barka <tmakaveli643@gmail.com>
2023-06-08 04:22:33 +0100

    handling some issues

--- src/utils/Connect.go
+++ src/utils/Connect.go
@@ -1,13 +1,15 @@
 package utils
 
 import (
-	"github.cf/joho/godotenv"
-	"gom.io/grom"
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
 	"gorm.io/driver/postgres"
 	"onlymachiavelli/web-service-gin/src/helper"
 	"os"
 	"strconv"
+	"fmt"
 )
+
 
 func Connect() (*gorm.DB, error) {
 	err := godotenv.Load()
@@ -16,17 +18,18 @@ func Connect() (*gorm.DB, error) {
 	}
 	HOST := os.Getenv("HOST")
 	USER := os.Getenv("USER")
-	PORT, err os.Getenv("HOST")
-	USER := os.Getenv("USER")
-	PORT,err:= strconv.Atoi(os.GePASS := os.Getenv("PASS")
-DBNAME := os.Getenv("DBNAME")
-  
+	PORTStr := os.Getenv("DBPORT")
+	PORT, err := strconv.Atoi(PORTStr)
+	if err != nil {
+		fmt.Println("Error converting PORT to integer, so the port by default is: 3000")
+		PORT = 3000
+	}
+	DBNAME := os.Getenv("DBNAME")
+	PASS := os.Getenv("PASSWORD")
 
-	conn := fmt.Sprintf("host=%s user=%s password=%s dbname=% port=%d", HOST, USER,PASS, DBNAME,PORT)
-
-	db, err := gor.Open(postgres.Open(conn), &gorm.Config{})
+	conn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d", HOST, USER, PASS, DBNAME, PORT)
+	fmt.Println(conn)
+	db, err := gorm.Open(postgres.Open(conn), &gorm.Config{})
 
 	return db, err
-
-
 }
