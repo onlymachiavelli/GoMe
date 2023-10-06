459aefa704e972e7b8c18cfabb924ca6bdf0acae
Lord Machiavelli <tmakaveli643@gmail.com>
2023-10-06 23:13:08 +0100

    adding mongodb

--- src/models/users.go
+++ src/models/users.go
@@ -1,19 +1,19 @@
-package models 
+// package models 
 
 
 
-import (
-	"gorm.io/gorm"
-)
+// import (
+// 	"gorm.io/gorm"
+// )
 
 
-type Users struct {
+// type Users struct {
 	
-	id int `gorm:"primaryKey;autoIncrement"`
-	fullname string
-	email string `gorm:"unique"`
-	password string
-	phone string `gorm:"unique"`
+// 	id int `gorm:"primaryKey;autoIncrement"`
+// 	fullname string
+// 	email string `gorm:"unique"`
+// 	password string
+// 	phone string `gorm:"unique"`
 
-	created_at string
-}
+// 	created_at string
+// }
