27a644b61ff9a756b6ddffe217edcd6204116621
Lord Machiavelli <tmakaveli643@gmail.com>
2023-10-12 00:14:57 +0100

    fixing bug (restarting the server)

--- main.go
+++ main.go
@@ -6,19 +6,30 @@ import (
 	"onlymachiavelli/web-service-gin/src/routes"
 	"github.com/gin-gonic/gin"
 	"context"
+	"log"
 )
 
 func main() {
+    r := gin.Default()
 
-	r := gin.Default()
+    client, err := utils.Connect()
+    if err != nil {
+        log.Fatalf("Error connecting to MongoDB: %v", err)
+        return
+    }
 	
-	client, err := utils.Connect()
-	if err != nil {
+    defer func() {
+        if err := client.Disconnect(context.TODO()); err != nil {
+            log.Printf("Error disconnecting from MongoDB: %v", err)
+        }
+    }()
+
+    routes.Routes(r)
+
+    // Start the Gin server
+    err = r.Run(); // Change the port as needed
+    if err != nil {
+        //use the helper 
 		helper.ErrorHandler(err)
-	}
-	routes.Routes(r)
-
-	
-	defer client.Disconnect(context.TODO())
-
+    }
 }
